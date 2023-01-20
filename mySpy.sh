d00a83b14639424bf43b3afb9d1a0ab02cec68d7
Frank Zheng <zmyfrank@qq.com>
2023-01-20 22:38:27 +0800

    feat: 新建项目

---
+++ mySpy.sh
@@ -0,0 +1,10 @@
+js_config:
+- Container:
+  - []
+  Env: jd_wxShopFollowActivity_activityUrl
+  KeyWord:
+  - - jd_wxShopFollowActivity_activityUrl
+  Name: 【抽奖】关注店铺抽奖
+  Script: jd_wxShopFollowActivity.js
+  TimeOut: 0
+  Wait: 2
